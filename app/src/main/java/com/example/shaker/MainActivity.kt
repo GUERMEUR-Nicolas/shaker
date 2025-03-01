f304879242d8d1ab8a2bb814d62c832377c7c4c0
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-02-28 20:32:15 -0500

    forwarded the state and used it in the composable to show the actual data

--- app/src/main/java/com/example/shaker/MainActivity.kt
+++ app/src/main/java/com/example/shaker/MainActivity.kt
@@ -6,21 +6,47 @@ import androidx.activity.compose.setContent
 import com.example.shaker.home.MainViewModel
 import com.example.shaker.ui.theme.ShakerTheme
 import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.shaker.home.CenterSidebarPager
 import com.example.shaker.ui.GameplayViewModel
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
 	private val viewModel: MainViewModel by viewModels()
+	private val gameplayState: GameplayViewModel by viewModels()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		//enableEdgeToEdge()
 		setContent {
 			ShakerTheme(darkTheme = false) {
-				CenterSidebarPager(viewModel)
+				CenterSidebarPager(viewModel,gameplayState)
+				LaunchedEffect(Unit) {
+					while (true) {
+						delay(160L) // Delay for 1 second
+						gameplayState.Increment(160L / 1000f)
+					}
+				}
+			}
+		}
+	}
+
+}
+@Preview(widthDp = 380, heightDp = 680)
+@Composable
+fun AppPreview(){
+	val viewModel = MainViewModel()
+	val gameplayState = GameplayViewModel()
+	ShakerTheme(darkTheme = false) {
+		CenterSidebarPager(viewModel,gameplayState)
+		LaunchedEffect(Unit) {
+			while (true) {
+				delay(160L) // Delay for 1 second
+				gameplayState.Increment(160L / 1000f)
 			}
 		}
 	}
 }
-
 data class TabItem(val name:String, val screen: String)
--- app/src/main/java/com/example/shaker/home/Pager.kt
+++ app/src/main/java/com/example/shaker/home/Pager.kt
@@ -18,11 +18,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.shaker.TabItem
-import com.example.shaker.data.upgrades
+import com.example.shaker.data.recipes
+import com.example.shaker.ui.GameplayViewModel
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-public fun CenterSidebarPager(viewModel: MainViewModel) {
+public fun CenterSidebarPager(viewModel: MainViewModel, gameplayState: GameplayViewModel) {
+
     val listTabItem = listOf(
         TabItem("home", "screen_0"),
         TabItem("upgrade", "screen_1")
@@ -32,17 +34,24 @@ public fun CenterSidebarPager(viewModel: MainViewModel) {
     ) { listTabItem.size } // Horizontal pages
     val pagerState_V: PagerState = rememberPagerState(
         initialPage = viewModel.selectedUpgradeId.value
-    ) { upgrades.size } // Vertical pages
+    ) { recipes.size } // Vertical pages
 
     Box(modifier = Modifier.fillMaxSize()) {
-        CurrentPage(viewModel, pagerState_H, pagerState_V)
-        MovingSideBar(viewModel, pagerState_H, pagerState_V, Modifier.align(Alignment.CenterEnd))
+        CurrentPage(viewModel, gameplayState, pagerState_H, pagerState_V)
+        MovingSideBar(
+            viewModel,
+            gameplayState,
+            pagerState_H,
+            pagerState_V,
+            Modifier.align(Alignment.CenterEnd)
+        )
     }
 }
 
 @Composable
 fun CurrentPage(
     viewModel: MainViewModel,
+    gameplayState: GameplayViewModel,
     pagerState_H: PagerState,
     pagerState_V: PagerState
 ) {
@@ -57,12 +66,13 @@ fun CurrentPage(
     ) { page ->
         selectedTabItem = page
         when (page) {
-            0 -> HomePage(Modifier.fillMaxWidth(0.8f))
+            0 -> HomePage(Modifier.fillMaxWidth(0.8f), gameplayState)
             1 -> UpgradePage(
                 pagerState_V,
                 { upgradeId ->
                     viewModel.selectUpgrade(upgradeId)
                 },
+                gameplayState,
                 Modifier
             )
 
