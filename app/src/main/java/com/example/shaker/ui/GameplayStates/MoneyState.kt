d4bf6e23cb138a045e1144cb6c5c88556aa3cb71
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-03-03 22:20:18 -0500

    Initial money is 1 so you can buy a first recipe

--- app/src/main/java/com/example/shaker/ui/GameplayStates/MoneyState.kt
+++ app/src/main/java/com/example/shaker/ui/GameplayStates/MoneyState.kt
@@ -3,7 +3,7 @@ package com.example.shaker.ui.GameplayStates
 import com.example.shaker.data.ScalingInt
 
 data class MoneyState(
-    val current : ScalingInt = ScalingInt(0),
+    val current : ScalingInt = ScalingInt(1),
     val perSecond : ScalingInt = ScalingInt(0),
     val perShake : ScalingInt = ScalingInt(10),
 )
