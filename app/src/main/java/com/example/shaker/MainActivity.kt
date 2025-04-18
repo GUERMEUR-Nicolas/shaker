12007e09932470cdc93da586198e56997c6c9396
superzero4 <78915984+superzero4@users.noreply.github.com>
2025-04-18 15:00:54 -0400

    Locked rotation in portrait

--- app/src/main/java/com/example/shaker/MainActivity.kt
+++ app/src/main/java/com/example/shaker/MainActivity.kt
@@ -1,25 +1,20 @@
 package com.example.shaker
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
-import com.example.shaker.home.MainViewModel
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.compose.AppTheme
 import com.example.shaker.data.allRecipes
-import com.example.shaker.home.CenterSidebarPager
 import com.example.shaker.home.Accelerometer
+import com.example.shaker.home.CenterSidebarPager
+import com.example.shaker.home.MainViewModel
 import com.example.shaker.home.ShakeListener
 import com.example.shaker.ui.GameplayViewModel
 import kotlinx.coroutines.delay
@@ -29,6 +24,7 @@ class MainActivity : ComponentActivity() {
     private val gameplayState: GameplayViewModel by viewModels()
     private val sensor: Accelerometer = Accelerometer()
 
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val shakeDelay: Long = resources.getInteger(R.integer.shakeDelayMilli).toLong()
@@ -45,6 +41,8 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge()
         val delay: Long = resources.getInteger(R.integer.shakeDelayMilli).toLong()
         setContent {
+            LocalActivity.current?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
             AppTheme(darkTheme = false, dynamicColor = false) {
                 CenterSidebarPager(viewModel, gameplayState, 0)
                 LaunchedEffect(Unit) {
